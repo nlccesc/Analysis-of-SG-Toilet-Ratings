1cc6a3cfb17d143d562c92e4dab7488d78843297
nlccesc <93630735+nlccesc@users.noreply.github.com>
2024-03-31 16:02:44 +0800

    Update statistical_testing.py

--- statistical_testing.py
+++ statistical_testing.py
@@ -2,12 +2,13 @@ import pandas as pd
 from scipy.stats import pearsonr
 
 
-file_path = r"C:\Users\haoha\OneDrive\Desktop\personal\Personal Projects\SG Toilet Ratings\Cleaned_SG_Toilet_Ratings.xlsx"
+file_path = r"file path of your excel/csv file"
 cleaned_data = pd.read_excel(file_path)
 cleaned_data['TotalScore'] = pd.to_numeric(cleaned_data['TotalScore'], errors='coerce')
 
 
-# Calculate the correlation coefficient between 'ScentScore' and 'TotalScore'
+# Calculate the correlation coefficient between one parameter and 'TotalScore'
+# I used scentscore here, but you can change it to something else
 # Ensure 'ScentScore' is converted to a numeric type if it isn't already
 cleaned_data['ScentScore'] = pd.to_numeric(cleaned_data['ScentScore'], errors='coerce')
 correlation, p_value = pearsonr(cleaned_data.dropna()['ScentScore'], cleaned_data.dropna()['TotalScore'])
